287a00ee1bb60cee7b40ac04075f20c35b9190a6
CitronFoisPi <163825036+CitronFoisPi@users.noreply.github.com>
2025-03-31 19:40:38 +0200

    Update graphe_de_ton_prenom.py pour aller avec le programme graphe_prenom_graphiquement.py

--- graphe_de_ton_prenom.py
+++ graphe_de_ton_prenom.py
@@ -1,7 +1,7 @@
 import csv
 import sqlite3 
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 plt.style.use('dark_background')
@@ -11,36 +11,39 @@ plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 liaison = sqlite3.connect("prenoms.db")
 curseur = liaison.cursor()
 
-
-curseur.execute("SELECT DISTINCT preusuel FROM prenoms;")
-
-result = curseur.fetchall()
-prenoms = set([p_uplet[0] for p_uplet in result])
-prenom = input("Entrer un prénom : ")
-sexe = int(input("Entrer le sexe (1 pour masculin, 2 pour féminin) : "))
-if prenom.upper() in prenoms:
-    couleurs = ('purple', 'gold')
-    curseur.execute("SELECT annais, nombre FROM prenoms WHERE preusuel LIKE ? AND sexe = ? ORDER BY annais ;", (prenom, sexe))
+def graphe_prenom(prenom: str, sexe: int):
+    curseur.execute("SELECT DISTINCT preusuel FROM prenoms;")
 
     result = curseur.fetchall()
+    prenoms = set([p_uplet[0] for p_uplet in result])
+    if prenom.upper() in prenoms:
+        plt.clf()
+        couleurs = ('purple', 'gold')
+        curseur.execute("SELECT annais, nombre FROM prenoms WHERE preusuel LIKE ? AND sexe = ? ORDER BY annais ;", (prenom, sexe))
 
-    x = [int(annee) for annee, nombre in result if annee != 'XXXX']
-    y = [int(nombre) for annee, nombre in result if annee != 'XXXX']
-    indice_max = y.index(max(y))
-    x_max = x[indice_max]
-    y_max = y[indice_max]
+        result = curseur.fetchall()
 
-    plt.ylim(0, max(y) * 1.1)  # Ajuste pour un peu d'espace en haut
+        x = [int(annee) for annee, nombre in result if annee != 'XXXX']
+        y = [int(nombre) for annee, nombre in result if annee != 'XXXX']
+        indice_max = y.index(max(y))
+        x_max = x[indice_max]
+        y_max = y[indice_max]
+
+        plt.ylim(0, max(y) * 1.1)  # Ajuste pour un peu d'espace en haut
 #plt.scatter(x, y, color='r', label="Points")
-    plt.plot(x, y, marker='o', linestyle='-', color=couleurs[0], label=prenom)
-    plt.scatter(x_max, max(y), marker='x', color='r', label=f"Max en x={x_max}", zorder=3)
-    plt.text(x_max, max(y), f"année ={x_max}, nb naissance ={y_max}", verticalalignment='bottom', fontsize=14, color=couleurs[1])
-    plt.xlabel("Années")
-    plt.ylabel("Naissances")
-    plt.title("Graphique avec abscisse du maximum")
-    plt.legend()
-    plt.show()
-    plt.savefig("graphique.png")
-else:
-    print("Ce prénom n'est pas dans la bdd")
-liaison.close()
+        plt.plot(x, y, marker='o', linestyle='-', color=couleurs[0], label=prenom)
+        plt.scatter(x_max, max(y), marker='x', color='r', label=f"Max en x={x_max}", zorder=3)
+        plt.text(x_max, max(y), f"année ={x_max}, nb naissance ={y_max}", verticalalignment='bottom', fontsize=14, color=couleurs[1])
+        plt.xlabel("Années")
+        plt.ylabel("Naissances")
+        plt.title("Graphique avec abscisse du maximum")
+        plt.legend()
+        #plt.show()
+        plt.savefig("graphique.png")
+        return True
+    else:
+        print("Ce prénom n'est pas dans la bdd")
+        return False
+    liaison.close()
+if __name__ == '__main__':
+    graphe_prenom("gabriel", 1)
