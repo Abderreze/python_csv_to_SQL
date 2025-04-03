97360b205ade39026c8ab8c4c5326ba91c6b2de4
CitronFoisPi <163825036+CitronFoisPi@users.noreply.github.com>
2025-04-03 11:57:56 +0200

    on peut ajouter et retirer des prénoms, chaque prénom à sa couleur

--- Graphes/graphe_prenoms_graphiquement.py
+++ Graphes/graphe_prenoms_graphiquement.py
@@ -1,43 +1,49 @@
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
-from graphe_de_ton_prenom import graphe_prenom
-def afficher_graphique():
-    prenom, sexe = search.get(), dropdown.get()
-    result = graphe_prenom(prenom, sexe)
-    if result == True:
-        image = Image.open("graphique.png")  # Remplace par le nom de ton fichier
-        image = image.resize((500, 400), Image.Resampling.LANCZOS)  # Ajuster la taille si besoin
-        photo = ImageTk.PhotoImage(image)
-    
-        label.config(image=photo)
-        label.image = photo  # Nécessaire pour éviter la suppression par le garbage collector
-def on_enter(event):
-    sexe = dropdown.get()
-    if sexe:
-        afficher_graphique()
-    else:
-        print("Sélectionnez un sexe")
-def on_select(event):
-    sexe = dropdown.get()
-# Création de la fenêtre principale
-root = tk.Tk()
-root.title("Affichage du Graphique")
-# Création du bouton
-btn = ttk.Button(root, text="Afficher le Graphique", command=afficher_graphique)
-btn.pack(pady=10)
-search = tk.StringVar()
-search_label = tk.Label(root, text="Entrez un prénom : ")
-entry = ttk.Entry(root, width=40, textvariable=search)
-entry.pack(pady=10)
-root.bind('<Return>', on_enter)
-options = [1, 2]
-dropdown = ttk.Combobox(values=options)
-dropdown.pack()
-# Label pour afficher l'image
-label = tk.Label(root)
-label.pack()
+import csv
+import sqlite3 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+plt.style.use('dark_background')
+plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
-# Lancement de l'interface
-root.mainloop()
+liaison = sqlite3.connect("prenoms.db")
+curseur = liaison.cursor()
 
+def graphe_prenom(prenoms_sexes: dict()):
+    plt.clf()
+    curseur.execute("SELECT DISTINCT preusuel FROM prenoms;")
+    result = curseur.fetchall()
+    prenoms = set([p_uplet[0] for p_uplet in result])
+    existe = True
+    for prenom_sexe, couleur in prenoms_sexes.items():
+        prenom, sexe = prenom_sexe
+        if prenom.upper() in prenoms:
+            curseur.execute("SELECT annais, nombre FROM prenoms WHERE preusuel LIKE ? AND sexe = ? ORDER BY annais ;", (prenom, sexe))
+
+            result = curseur.fetchall()
+
+            x = [int(annee) for annee, nombre in result if annee != 'XXXX']
+            y = [int(nombre) for annee, nombre in result if annee != 'XXXX']
+            indice_max = y.index(max(y))
+            x_max = x[indice_max]
+            y_max = y[indice_max]
+            plt.text(x_max, min(y), f"{int(x_max)}", color='red', fontsize=12, verticalalignment='bottom', horizontalalignment='right')
+            plt.text(x[0], y_max, f"{y_max}", color='red', fontsize=12, verticalalignment='bottom', horizontalalignment='right')
+            plt.plot(x, y, marker=None, linestyle='-', color=couleur, label=prenom)
+            plt.scatter(x_max, max(y), marker='x', color='r', label=f"Max en x={x_max}", zorder=3)
+            plt.xlabel("Années")
+            plt.ylabel("Naissances")
+            plt.title("Graphique avec abscisse du maximum")
+            plt.legend()
+            existe = (True and existe)
+        else:
+            print("Ce prénom n'est pas dans la bdd")
+    #        curseur.execute("SELECT DISTINCT preusuel FROM prenoms WHERE ")
+            existe = (False and existe)
+
+    plt.savefig("graphique.png")
+    return existe
+if __name__ == '__main__':
+    graphe_prenom("gabriel", 1)
